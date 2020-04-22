902eb4c9be98f96d76b90efb61097f64cb099a85
Rahul Handoo <handoo.r@northeastern.edu>
2020-04-22 11:46:40 -0400

    Remove api key

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,5 @@
 const url =
-  'https://newsapi.org/v2/top-headlines?country=us&apiKey=2abca9b2ced44586a5c18e282f890392'
+  'https://newsapi.org/v2/top-headlines?country=us&apiKey=<your_api_key>'
 
 export async function getNews () {
   let result = await fetch(url).then(response => response.json())
